bf37bc56e1f91aac61158972ae054676871e1fba
Vishalkapoor123 <vishalkapoorbkn@gmail.com>
2020-11-13 11:46:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-w471nu-jgmydz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-w471nu-jgmydz)
---
+++ src/app/progress-bar-determinate-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Determinate progress-bar
+ */
+@Component({
+  selector: 'progress-bar-determinate-example',
+  templateUrl: 'progress-bar-determinate-example.html',
+})
+export class ProgressBarDeterminateExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
